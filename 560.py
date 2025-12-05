51e140c3658f42b216ab21f472c3baefcdaadf90
voitsytskiyivan-creator <voitsytskiyivan@gmail.com>
2025-12-05 11:10:29 +0200

    Delete 560.py

--- 560.py
+++
@@ -1,8 +0,0 @@
-text = input("Введіть текст: ")
-text = text.lower()
-counts = [0] * 26
-for ch in text:
-    if 'a' <= ch <= 'z':
-        index = ord(ch) - ord('a')
-        counts[index] += 1
-print(f"Кількість букв в тексті: {counts}")
